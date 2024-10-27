c96832eb81f77ba8a225f4fec1f32202bdc2952f
whisperky <josephcollins1210@gmail.com>
2024-10-27 17:37:45 -0400

    chore: Change url of thumbnails.

--- src/services/tempData.ts
+++ src/services/tempData.ts
@@ -17,56 +17,57 @@ export const sampleVideos: Video[] = [
   {
     id: 2,
     title: "Elephant's Dream",
-    url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",    
-    thumbnail: "https://peach.blender.org/wp-content/uploads/bbb-splash.png", // Add thumbnail
+    url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+    thumbnail:
+      "https://upload.wikimedia.org/wikipedia/commons/e/e4/Elephants_Dream_cover.jpg", // Add thumbnail
     year: "2006",
   },
   {
     id: 3,
     title: "Sintel",
-    url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4",    
+    url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4",
     thumbnail: "https://peach.blender.org/wp-content/uploads/bbb-splash.png", // Add thumbnail
     year: "2010",
   },
   {
     id: 4,
     title: "Tears of Steel",
-    url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4",    
+    url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4",
     thumbnail: "https://peach.blender.org/wp-content/uploads/bbb-splash.png", // Add thumbnail
     year: "2012",
   },
   {
     id: 5,
     title: "NASA Earth Views",
-    url: "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4",    
+    url: "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4",
     thumbnail: "https://peach.blender.org/wp-content/uploads/bbb-splash.png", // Add thumbnail
     year: "2014",
   },
   {
     id: 6,
     title: "NASA Earth Views",
-    url: "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4",    
+    url: "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4",
     thumbnail: "https://peach.blender.org/wp-content/uploads/bbb-splash.png", // Add thumbnail
     year: "2014",
   },
   {
     id: 7,
     title: "NASA Earth Views",
-    url: "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4",    
+    url: "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4",
     thumbnail: "https://peach.blender.org/wp-content/uploads/bbb-splash.png", // Add thumbnail
     year: "2014",
   },
   {
     id: 8,
     title: "NASA Earth Views",
-    url: "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4",    
+    url: "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4",
     thumbnail: "https://peach.blender.org/wp-content/uploads/bbb-splash.png", // Add thumbnail
     year: "2014",
   },
   {
     id: 9,
     title: "NASA Earth Views",
-    url: "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4",    
+    url: "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_1MB.mp4",
     thumbnail: "https://peach.blender.org/wp-content/uploads/bbb-splash.png", // Add thumbnail
     year: "2014",
   },
