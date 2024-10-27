076dc99e0b7179f5fa5ad95724f2d3089ac35cdc
whisperky <josephcollins1210@gmail.com>
2024-10-27 16:51:21 -0400

    feat: Add redirecting to /login page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
 "use client";
 
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/login");
+  }, [router]);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
   );
